a1ce0fb84e5445f24b78a9ca2e8e82580db7ddbe
Tokscull <egor.kirilenko.99@gmail.com>
2019-04-14 18:32:59 +0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SaleComponent } from './components/sale/sale.component';
+import { RentComponent } from './components/rent/rent.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRouter: Routes = [
+  {path: '', component: RentComponent},
+  {path: 'sale', component: SaleComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaleComponent,
+    RentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
